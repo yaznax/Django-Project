17875f8c16bb83a9019200836b0f76e69deb2c6d
Sam Smith <sanjayshrestha4747@gmail.com>
2021-06-22 21:33:34 +0545

    'functional'

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import home
+from .views import *
 app_name="home"
 urlpatterns = [
-    path('', home, name='home'),
+    path('', index, name='index'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('bloghome', bloghome, name='bloghome'),
+    path('blogsingle', blogsingle, name='blogsingle'),
+    path('elements', elements, name='elements'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
+    
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def index(request):
 	return render(request,'index.html')
+
+def about(request):
+	return render(request,'about.html')
+
+def contact(request):
+	return render(request,'contact.html')
+
+def bloghome(request):
+	return render(request,'blog-home.html')
+
+def blogsingle(request):
+	return render(request,'blog-single.html')
+
+def elements(request):
+	return render(request,'elements.html')
+
+def portfolio(request):
+	return render(request,'portfolio.html')
+
+def price(request):
+	return render(request,'price.html')
+
+def services(request):
+	return render(request,'services.html')

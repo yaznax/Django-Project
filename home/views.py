19dadf12aefaf929cebb96cab63b9671e9c933f3
Sam Smith <sanjayshrestha4747@gmail.com>
2021-06-28 22:24:55 +0545

    'views'

--- home/views.py
+++ home/views.py
@@ -16,22 +16,29 @@ def about(request):
 	return render(request,'about.html',view)
 
 def contact(request):
+    	view = views()
 	return render(request,'contact.html')
 
 def bloghome(request):
+	view = views()
 	return render(request,'blog-home.html')
 
 def blogsingle(request):
+    	view = views()
 	return render(request,'blog-single.html')
 
 def elements(request):
+    	view = views()
 	return render(request,'elements.html')
 
 def portfolio(request):
+    	view = views()
 	return render(request,'portfolio.html')
 
 def price(request):
+    	view = views()
 	return render(request,'price.html')
 
 def services(request):
+    	view = views()
 	return render(request,'services.html')
